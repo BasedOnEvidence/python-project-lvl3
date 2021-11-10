bbd1efab53e8440c03aeb4c15d70bbd2eb53dab3
BasedOnEvidence <TestTypoTest@gmail.com>
2021-11-10 21:58:29 +0300

    Add FakeResponse class security

--- tests/fake_response.py
+++ tests/fake_response.py
@@ -1,4 +1,12 @@
 class FakeResponse:
     def __init__(self, content=b'', text=''):
-        self.content = content
-        self.text = text
+        self._content = content
+        self._text = text
+
+    @property
+    def content(self):
+        return self._content
+
+    @property
+    def text(self):
+        return self._text
