e8669442a78f03185948505322e86f26818c614e
BasedOnEvidence <TestTypoTest@gmail.com>
2021-11-28 22:32:36 +0300

    Add logger

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -7,7 +7,10 @@ from page_loader.os_tools import (
     get_content_folder_name,
     get_file_name_from_url
 )
+from page_loader.logger import get_logger
 
+
+logger = get_logger(__name__)
 
 RESOURCES = {
     'img': 'src',
@@ -50,7 +53,6 @@ def download_content(txt_data, output_path, content_path, base_url):
     for tag in RESOURCES.keys():
         for url in urls[tag]:
             if is_url_in_domain(domain, url):
-                print(url)
                 file_name = get_file_name_from_url(url)
                 content_file_path = content_path + '/' + file_name
                 current_txt = current_txt.replace(url, content_file_path)
@@ -70,6 +72,8 @@ def download(output_path, url):
     file_name = convert_url_to_file_name(url)
     content_folder_name = get_content_folder_name(file_name)
     content_path = output_path + '/' + content_folder_name
+    logger.info('Content path: {}'.format(content_path))
     output_path += '/' + file_name
+    logger.info('Output path: {}'.format(output_path))
     download_content(txt_data, output_path, content_path, url)
     return output_path
--- page_loader/scripts/run.py
+++ page_loader/scripts/run.py
@@ -5,8 +5,7 @@ from page_loader.page_loader import download
 def main():
     parser = get_args_parser()
     args = parser.parse_args()
-    file_path = download(args.output, args.link)
-    print(file_path)
+    download(args.output, args.link)
 
 
 if __name__ == '__main__':
