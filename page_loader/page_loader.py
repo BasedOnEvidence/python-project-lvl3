1562b3181ffde4e707072c6f858cabfa0b9b6634
BasedOnEvidence <TestTypoTest@gmail.com>
2021-11-30 11:11:05 +0300

    Add soup.prettify for output

--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -22,39 +22,30 @@ RESOURCES = {
 }
 
 
-def get_resources(txt_data):
-    soup = BeautifulSoup(txt_data, 'html.parser')
-    data = {}
-    for tag in RESOURCES.keys():
-        tags = soup.find_all(tag, {RESOURCES[tag]: True})
-        urls = [item[RESOURCES[tag]] for item in tags]
-        data[tag] = urls
-    return data
+def get_url(resource, base_url):
+    url = resource[RESOURCES[resource.name]]
+    return make_url_absolute(base_url, url)
 
 
-def download_resources(txt_data, output_path, content_path, base_url):
+def download_resources(resources, content_path, base_url):
     logger.info('Current base url: {}'.format(base_url))
-    current_txt = txt_data
     domain = get_domain_from_url(base_url)
-    urls = get_resources(txt_data)
-    logger.info('Urls: {}'.format(urls))
-    bar_max_length = sum([len(item) for item in urls.values()])
+    logger.info('Tags: {}'.format(resources))
+    bar_max_length = len(resources)
     logger.info('Items count: {}'.format(bar_max_length))
     bar = ChargingBar('Downloading resources:', max=bar_max_length)
-    for tag in RESOURCES.keys():
-        for url in urls[tag]:
-            if is_url_in_domain(domain, url):
-                file_name = get_file_name_from_url(url)
-                resource_path = os.path.join(content_path, file_name)
-                current_txt = current_txt.replace(url, resource_path)
-                with open(resource_path, 'wb') as f:
-                    url = make_url_absolute(base_url, url)
-                    logger.info('Current content url: {}'.format(url))
-                    response = requests.get(url)
-                    f.write(response.content)
-            bar.next()
-    with open(output_path, 'w') as file_:
-        file_.write(current_txt)
+    for resource in resources:
+        url = get_url(resource, base_url)
+        if is_url_in_domain(domain, url):
+            file_name = get_file_name_from_url(url)
+            resource_path = os.path.join(content_path, file_name)
+            logger.info('Current resource path: {}'.format(resource_path))
+            with open(resource_path, 'wb') as f:
+                logger.info('Current content url: {}'.format(url))
+                response = requests.get(url)
+                f.write(response.content)
+            resource[RESOURCES[resource.name]] = resource_path
+        bar.next()
     bar.finish()
 
 
@@ -67,5 +58,9 @@ def download(url, output_path):
     os.mkdir(resources_path)
     logger.info('File path: {}'.format(file_path))
     logger.info('Resources path: {}'.format(resources_path))
-    download_resources(response.text, file_path, resources_path, url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    resources = soup.find_all(RESOURCES.keys())
+    download_resources(resources, resources_path, url)
+    with open(file_path, 'w') as f:
+        f.write(soup.prettify(formatter="html5"))
     return file_path
