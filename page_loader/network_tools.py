1d5e56acddd7b8723ee35e167031028b7fd158a5
BasedOnEvidence <TestTypoTest@gmail.com>
2021-12-16 19:21:57 +0300

    Fix status code check

--- page_loader/network_tools.py
+++ page_loader/network_tools.py
@@ -10,7 +10,8 @@ logger = get_logger(__name__)
 def get_page_obj(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()
+        if response.status_code > 400:
+            raise requests.HTTPError
         return BeautifulSoup(response.text, 'html.parser')
     except requests.exceptions.RequestException as err:
         logger.error(err)
@@ -19,7 +20,8 @@ def get_page_obj(url):
 def download_resource_item(url, path):
     try:
         response = requests.get(url)
-        response.raise_for_status()
+        if response.status_code > 400:
+            raise requests.HTTPError
     except requests.exceptions.RequestException as err:
         logger.error(err)
     logger.debug('{} is downloaded'.format(url))
