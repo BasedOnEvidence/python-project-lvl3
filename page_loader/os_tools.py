a50770405b27adbc915646d9b91a1d92cc850d34
BasedOnEvidence <TestTypoTest@gmail.com>
2022-01-01 20:41:22 +0300

    Change exceptions location

--- page_loader/os_tools.py
+++ page_loader/os_tools.py
@@ -7,13 +7,6 @@ logger = get_logger(__name__)
 
 
 def save_file(path, file, mode='wb'):
-    try:
-        with open(path, mode) as f:
-            f.write(file)
-        logger.debug('{} is created'.format(path))
-    except FileNotFoundError as err:
-        logger.error('No such file or directory!')
-        raise FileNotFoundError(err)
-    except PermissionError as err:
-        logger.error('Access denied!')
-        raise PermissionError(err)
+    with open(path, mode) as f:
+        f.write(file)
+    logger.debug('{} is created'.format(path))
--- page_loader/page_loader.py
+++ page_loader/page_loader.py
@@ -18,16 +18,23 @@ def download(url, output_path):
     res_path = os.path.join(output_path, url_tools.to_dir_name(url, '_files'))
     response = make_request(url)
     html, resources = process_html(response, url)
-    save_file(file_path, html, mode='w')
-    if not os.path.exists(res_path) and resources:
-        os.mkdir(res_path)
-        logger.debug('{} is created'.format(res_path))
-    bar = ChargingBar('Downloading resources:', max=len(resources))
-    for res_url in resources:
-        file_obj = download_resource_item(res_url)
-        res_file_name = url_tools.to_file_name(res_url)
-        res_file_path = os.path.join(res_path, res_file_name)
-        save_file(res_file_path, file_obj)
-        bar.next()
-    bar.finish()
-    return file_path
+    try:
+        save_file(file_path, html, mode='w')
+        if not os.path.exists(res_path) and resources:
+            os.mkdir(res_path)
+            logger.debug('{} is created'.format(res_path))
+        bar = ChargingBar('Downloading resources:', max=len(resources))
+        for res_url in resources:
+            file_obj = download_resource_item(res_url)
+            res_file_name = url_tools.to_file_name(res_url)
+            res_file_path = os.path.join(res_path, res_file_name)
+            save_file(res_file_path, file_obj)
+            bar.next()
+        bar.finish()
+        return file_path
+    except FileNotFoundError as err:
+        logger.error('No such file or directory!')
+        raise FileNotFoundError(err)
+    except PermissionError as err:
+        logger.error('Access denied!')
+        raise PermissionError(err)
