67c43d1bfd83e16d0b6d4a0db43f6225a74d3a62
BasedOnEvidence <TestTypoTest@gmail.com>
2022-01-04 18:16:04 +0300

    Fix namer.py

--- page_loader/namer.py
+++ page_loader/namer.py
@@ -5,13 +5,18 @@ from urllib.parse import urlparse
 FORMAT_URL_PATTERN = r'[^A-Za-z0-9]'
 
 
+def get_ext(url):
+    url, ext = os.path.splitext(url)
+    if ext == '':
+        ext = '.html'
+    if not ext.startswith('.'):
+        ext = '.' + ext
+    return ext
+
+
 def to_file_name(url, force_ext=''):
-    if force_ext != '_files' and force_ext == '':
-        url, force_ext = os.path.splitext(url)
-        if force_ext == '':
-            force_ext = '.html'
-        if not force_ext.startswith('.'):
-            force_ext = '.' + force_ext
+    if force_ext == '':
+        force_ext = get_ext(url)
     parsed_url = urlparse(url)
     changed_url = re.sub(
         FORMAT_URL_PATTERN, '-', parsed_url.netloc + parsed_url.path
